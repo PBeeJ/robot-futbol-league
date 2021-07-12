f52e578b3f9e44088258b820d135957551f1b2f8
Bee <bee.wilkerson@ymail.com>
2021-07-12 06:08:52 -0700

    add support for compass heading updates from bots

--- src/ball-bot/ball-bot.py
+++ src/ball-bot/ball-bot.py
@@ -6,9 +6,14 @@ import json
 import asyncio
 import websockets
 
-GAME_CONTROLLER_URI = "ws://localhost:6789"
+from random import randrange
 
+
+GAME_CONTROLLER_URI = "ws://localhost:6789"
 GAME_BOUNDS = [-8, 4, 8, -4]
+GAME_STATE = None
+
+COMPASS_BEARING = 0
 
 
 async def recvLoop(websocket):
@@ -17,7 +22,25 @@ async def recvLoop(websocket):
         data = json.loads(message)
         messageType = data.get("type")
         messageData = data.get("data")
+        if messageType == "state":
+            GAME_STATE = messageData
+        elif messageType == "bounds":
+            GAME_BOUNDS = "bounds"
+
         print(f"got message {messageType} with {messageData}")
+
+
+async def sendCompassHeading(websocket):
+    message = json.dumps({
+        "type": "heading",
+        "data": {
+            "heading": randrange(360),
+            # TODO : remove this should not be needed
+            "botIndex": 0
+        }
+    })
+    print(f"sending compass heading {message}")
+    await websocket.send(message)
 
 
 async def mainLoop(websocket):
@@ -25,20 +48,21 @@ async def mainLoop(websocket):
     await asyncio.sleep(1)
 
     while True:
-        input('press a key')
-        await websocket.send(json.dumps({"type": "state"}))
-        await asyncio.sleep(1)
+        await sendCompassHeading(websocket)
+        await asyncio.sleep(.05)
+
+        # TODO : handle player intersection ball bot movement here
 
 
 async def ballBot():
     async with websockets.connect(GAME_CONTROLLER_URI) as websocket:
         recvTask = asyncio.create_task(recvLoop(websocket))
-        inputTask = asyncio.create_task(mainLoop(websocket))
+        movementTask = asyncio.create_task(mainLoop(websocket))
 
         # start both tasks, but have the loop return to us when one of them
         # has ended. We can then cancel the remainder
         done, pending = await asyncio.wait(
-            [recvTask, inputTask],
+            [recvTask, movementTask],
             return_when=asyncio.FIRST_COMPLETED
         )
         for task in pending:
--- src/game-controller/server.py
+++ src/game-controller/server.py
@@ -5,7 +5,19 @@ import json
 import asyncio
 import websockets
 
-ADMIN_IP_ADDRS = ['192.168.1.8', '192.168.1.9']
+from enum import Enum
+
+ADMIN_IP_ADDRS = ['::1', '192.168.1.8', '192.168.1.9']
+BALL_IP_ADDR = '192.168.1.3'
+PLAYER_1_IP_ADDR = '192.168.1.4'
+PLAYER_2_IP_ADDR = '192.168.1.5'
+
+
+class BOT_INDEX(Enum):
+    ball_bot: 0
+    player_1: 1
+    player_2: 2
+
 
 logging.basicConfig()
 
@@ -14,27 +26,30 @@ GAME_STATE = {
         "state": "GAME_OVER",
         "secondsRemaining": 0,
     },
-    "ballBot": {
+    "bots": [{
         "name": "ball-bot",
         "x": 0,
         "y": 0,
         "heading": 0
-    },
-    "players": [
-        {
-            "name": "player-1",
-            "x": 0,
-            "y": 0,
-            "heading": 0,
-        },
-        {
-            "name": "player-2",
-            "x": 0,
-            "y": 0,
-            "heading": 0,
-        }
-    ],
+    }, {
+        "name": "player-1",
+        "x": 0,
+        "y": 0,
+        "heading": 0,
+    }, {
+        "name": "player-2",
+        "x": 0,
+        "y": 0,
+        "heading": 0,
+    }],
     "scores": [0, 0],
+    "isDirty": False,
+}
+
+KNOWN_BOTS = {
+    BALL_IP_ADDR: GAME_STATE["bots"][0],
+    PLAYER_1_IP_ADDR: GAME_STATE["bots"][1],
+    PLAYER_2_IP_ADDR: GAME_STATE["bots"][2]
 }
 
 # aabb of playing field
@@ -58,7 +73,8 @@ async def send_message(websocket, message):
         await asyncio.wait([websocket.send(message) for websocket in SOCKETS])
 
 
-async def notify_state(websocket):
+async def notify_state(websocket="all"):
+    print(f"sending state to {websocket}")
     await send_message(websocket, state_message())
 
 
@@ -78,6 +94,19 @@ async def unregister(websocket):
     SOCKETS.remove(websocket)
 
 
+def getKnownBot(websocket, data):
+    remoteIp = websocket.remote_address[0]
+
+    # admins can specify `botIndex` in the messages
+    # that update state
+    if remoteIp in ADMIN_IP_ADDRS:
+        botIndex = data.get('botIndex')
+        if botIndex != None:
+            return GAME_STATE["bots"][botIndex]
+
+    return KNOWN_BOTS.get(remoteIp)
+
+
 async def handleStateRequest(websocket, data):
     if websocket.remote_address[0] in ADMIN_IP_ADDRS:
         # TODO : if comming from an admin apply to GAME_STATE
@@ -88,14 +117,25 @@ async def handleStateRequest(websocket, data):
 
 async def handleBoundsRequest(websocket, data):
     if websocket.remote_address[0] in ADMIN_IP_ADDRS:
-        print(f"got admin state request with {data}")
+        print(f"got admin bounds request with {data}")
         # TODO : if comming from an admin apply to GAME_STATE
 
     await notify_bounds(websocket)
 
 
-async def counter(websocket, path):
-    # register(websocket) sends user_event() to websocket
+async def handleCompassMessage(websocket, data):
+    knownBot = getKnownBot(websocket, data)
+    remoteAddr = websocket.remote_address[0]
+    heading = data.get("heading")
+    if knownBot and heading:
+        print(f"got heading message from known bot {knownBot}")
+        knownBot["heading"] = heading
+        GAME_STATE["isDirty"] = True
+    else:
+        print(f"got heading message from unknown bot {remoteAddr} (ignoring)")
+
+
+async def handleMessage(websocket, path):
     await register(websocket)
     try:
         async for message in websocket:
@@ -109,13 +149,25 @@ async def counter(websocket, path):
                 await handleStateRequest(websocket, messageData)
             elif messageType == "bounds":
                 await handleBoundsRequest(websocket, messageData)
+            elif messageType == "heading":
+                await handleCompassMessage(websocket, messageData)
             else:
                 logging.error("unsupported event: %s", jsonData)
     finally:
         await unregister(websocket)
 
 
-start_server = websockets.serve(counter, port=6789)
+async def send_state_task():
+    while True:
+        if GAME_STATE["isDirty"]:
+            GAME_STATE["isDirty"] = False
+            await notify_state()
+        await asyncio.sleep(0)
 
+
+start_server = websockets.serve(handleMessage, port=6789)
 asyncio.get_event_loop().run_until_complete(start_server)
+
+asyncio.get_event_loop().create_task(send_state_task())
+
 asyncio.get_event_loop().run_forever()
