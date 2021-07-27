26659ac6424686f0003e2e23dac5e365c98f2e78
Phineas Kibbey <pkibbey@liveperson.com>
2021-07-26 20:13:13 -0700

    Make playing piece size relative to field size

--- src/scoreboard/src/containers/gameField.js
+++ src/scoreboard/src/containers/gameField.js
@@ -7,9 +7,9 @@ import { green, red, blue } from "@material-ui/core/colors";
 import { Box } from "@material-ui/core";
 import ball from "../images/ball.svg";
 
-const iconSize = 30;
 const gridLines = 20;
 const gridTemplateColumns = "1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr";
+const playerDiameterInCM = 21;
 
 function FieldCell() {
   return <div style={{ outline: "1px solid rgba(255,255,255,0.5)" }} />;
@@ -23,12 +23,12 @@ function FieldGrid() {
   );
 }
 
-function getPosition(value) {
+function getPosition(value, offset) {
   // Return 0, if the value is not a number
   if (Number.isNaN(value)) return 0;
 
   // Return value, adjusted for icon size
-  return `${value - (iconSize / 2)}px`;
+  return `${value - (offset / 2)}px`;
 }
 
 const GameField = ({ gameState, gameConfig }) => {
@@ -37,20 +37,52 @@ const GameField = ({ gameState, gameConfig }) => {
   // Calculate field dimensions
   const fieldCenterX = bounds.width / 2;
   const fieldCenterY = bounds.height / 2;
-  const fieldLength = gameConfig.bounds[2] - gameConfig.bounds[0];
-  const fieldWidth = gameConfig.bounds[1] - gameConfig.bounds[3];
-  const fieldLengthUnit = bounds.width / fieldLength;
-  const fieldWidthUnit = bounds.height / fieldWidth;
+  const fieldWidth = gameConfig.bounds[2] - gameConfig.bounds[0];
+  const fieldHeight = gameConfig.bounds[1] - gameConfig.bounds[3];
+  const fieldWidthUnit = bounds.width / fieldWidth;
+  const fieldHeightUnit = bounds.height / fieldHeight;
+  const pieceProportion = playerDiameterInCM / gameConfig.centimetersPerUnit;
+  const pieceHeight = pieceProportion * fieldHeightUnit;
+  const pieceWidth = pieceProportion * fieldWidthUnit;
 
   // Calculate ball position
-  const ballX = getPosition(gameState.bots[0].x * fieldLengthUnit + fieldCenterX);
-  const ballY = getPosition(-1 * gameState.bots[0].y * fieldWidthUnit + fieldCenterY);
+  const ballX = getPosition(gameState.bots[0].x * fieldWidthUnit + fieldCenterX, pieceWidth);
+  const ballY = getPosition(-1 * gameState.bots[0].y * fieldHeightUnit + fieldCenterY, pieceHeight);
 
   // Calculate player positions
-  const bot1X = getPosition(gameState.bots[1].x * fieldLengthUnit + fieldCenterX);
-  const bot1Y = getPosition(-1 * gameState.bots[1].y * fieldWidthUnit + fieldCenterY);
-  const bot2X = getPosition(gameState.bots[2].x * fieldLengthUnit + fieldCenterX);
-  const bot2Y = getPosition(-1 * gameState.bots[2].y * fieldWidthUnit + fieldCenterY);
+  const bot1X = getPosition(gameState.bots[1].x * fieldWidthUnit + fieldCenterX, pieceWidth);
+  const bot1Y = getPosition(-1 * gameState.bots[1].y * fieldHeightUnit + fieldCenterY, pieceHeight);
+  const bot2X = getPosition(gameState.bots[2].x * fieldWidthUnit + fieldCenterX, pieceWidth);
+  const bot2Y = getPosition(-1 * gameState.bots[2].y * fieldHeightUnit + fieldCenterY, pieceHeight);
+
+  function Ball() {
+    return (
+      <img
+        alt=""
+        src={ball}
+        width={pieceWidth || 0}
+        height={pieceHeight || 0}
+        style={{
+          position: "absolute",
+          left: ballX,
+          top: ballY,
+        }}
+      />
+    );
+  }
+
+  function PlayerPiece(styleProps) {
+    return (
+      <Box
+        width={pieceWidth || 0}
+        height={pieceHeight || 0}
+        style={{
+          position: "absolute",
+          ...styleProps,
+        }}
+      />
+    );
+  }
 
   return (
     <>
@@ -64,37 +96,9 @@ const GameField = ({ gameState, gameConfig }) => {
         }}
         ref={containerRef}
       >
-        <img
-          alt=""
-          src={ball}
-          width={iconSize}
-          height={iconSize}
-          style={{
-            position: "absolute",
-            left: ballX,
-            top: ballY,
-          }}
-        />
-        <Box
-          width={iconSize}
-          height={iconSize}
-          style={{
-            position: "absolute",
-            left: bot1X,
-            top: bot1Y,
-            backgroundColor: red[400],
-          }}
-        />
-        <Box
-          width={iconSize}
-          height={iconSize}
-          style={{
-            position: "absolute",
-            left: bot2X,
-            top: bot2Y,
-            backgroundColor: blue[400],
-          }}
-        />
+        <Ball />
+        <PlayerPiece left={bot1X} top={bot1Y} backgroundColor={red[400]} />
+        <PlayerPiece left={bot2X} top={bot2Y} backgroundColor={blue[400]} />
         <FieldGrid />
       </div>
     </>
